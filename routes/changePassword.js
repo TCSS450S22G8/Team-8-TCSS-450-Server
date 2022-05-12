3e3da6158d19bd116c44e33d6b78ff1ffd46a858
slogansUW <slogans@uw.edu>
2022-05-11 22:11:37 -0700

    updated changePassword for JWT implementation

--- routes/changePassword.js
+++ routes/changePassword.js
@@ -1,138 +1,150 @@
 /**
  * Endpoint is used for changing a users password.
- * 
+ *
  * @author Sean Logan
  * @author Shilnara Dam
  * @version 1.0
  */
 
 //express is the framework we're going to use to handle requests
-const express = require('express');
+const express = require("express");
 
-const router = express.Router()
+const jwt = require("jsonwebtoken");
+
+const router = express.Router();
 
 // Utility functions for password generation/validation
-const { generateHash, generateSalt } = require('../utilities');
-const { isValidPassword } = require('../utilities/validationUtils')
+const { generateHash, generateSalt } = require("../utilities");
+const { isValidPassword } = require("../utilities/validationUtils");
 
 //Access the connection to Heroku Database
-const pool = require('../utilities').pool
-
+const pool = require("../utilities").pool;
 
 /**
  * @api {post} /change-password Request to update users password
  * @apiName postChangePassword
  * @apiGroup Change Password
- * 
- * 
+ *
+ * @apiParam {Number} memberid
+ * @apiParam {String} oldPassword
+ * @apiParam {String} newPassword
+ *
+ * @apiParamExample {json} Request-Example:
+ *           {
+ *            "jwt": "2i234y34h3khjh3k4h3",
+ *            "oldPassword": "1234Test",
+ *            "newPassword": "1234Test!!"
+ *           }
+ *
+ *
  * @apiSuccess {String} Password updated
- * 
+ *
  *  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
  *     {
  *         "message": "Password updated"
  *     }
- * 
- * @apiError (400: Invalid New Password) {String} message "Password does not meet requirements" 
- * 
+ *
+ * @apiError (400: Invalid New Password) {String} message "Password does not meet requirements"
+ *
  * @apiError (404: User Not Found) {String} message "User not found"
- * 
+ *
  * @apiError (400: Error Occured) {String} message "You should never reach this"
  *
  * @apiError (500: Internal Sever Error) {String} message "Database query failed"
- *  
+ *
  * @apiError (400: Invalid Old Password) {String} message "Invalid old password"
  *
- */ 
-router.post('/', (request, response, next) => {
-    const email = request.body.email
-    const oldPassword = request.body.oldPassword
-    const newPassword = request.body.newPassword
+ */
+router.post("/", (request, response, next) => {
+  const jwtBody = request.body.jwt;
+  const jwtDecoded = jwt.decode(jwtBody);
+  const memberid = jwtDecoded.memberid;
+  const oldPassword = request.body.oldPassword;
+  const newPassword = request.body.newPassword;
 
-    //Verify that the user supplied a valid new password
-    if(!isValidPassword(newPassword)) {
-        response.status(400).send({
-            message: 'Password does not meet requirements'
-        })
-        return
-    }
+  //Verify that the user supplied a valid new password
+  if (!isValidPassword(newPassword)) {
+    response.status(400).send({
+      message: "Password does not meet requirements",
+    });
+    return;
+  }
 
-    // Gets users salted hash, salt, and memberid
-    let getSaltQuery = "SELECT CREDENTIALS.SALTEDHASH, CREDENTIALS.SALT, CREDENTIALS.MEMBERID FROM MEMBERS " +
-                   "INNER JOIN CREDENTIALS ON MEMBERS.MEMBERID = CREDENTIALS.MEMBERID " +
-                   "WHERE UPPER(MEMBERS.EMAIL) = UPPER($1)"
+  // Gets users salted hash, salt, and memberid
+  let getSaltQuery =
+    "SELECT CREDENTIALS.SALTEDHASH, CREDENTIALS.SALT FROM CREDENTIALS WHERE CREDENTIALS.MEMBERID = $1";
 
-    const values = [request.body.email]
-    pool.query(getSaltQuery, values)
-        .then(result => {
-            // If user email doesnt exist, return error
-            if (result.rowCount == 0) {
-                response.status(404).send({
-                    message: 'User not found'
-                })
-                return
-            }
+  const values = [memberid];
+  pool
+    .query(getSaltQuery, values)
+    .then((result) => {
+      // If user email doesnt exist, return error
+      if (result.rowCount == 0) {
+        response.status(404).send({
+          message: "User not found",
+        });
+        return;
+      }
 
-            const memberid = result.rows[0].memberid
-            // If the user does exist, get salt and salted hash to 
-            // compare with old password that was sent
-            if (result.rowCount == 1) {
-                const salt = result.rows[0].salt
-                const salted_hash = generateHash(oldPassword, salt)
+      // If the user does exist, get salt and salted hash to
+      // compare with old password that was sent
+      if (result.rowCount == 1) {
+        const salt = result.rows[0].salt;
+        const salted_hash = generateHash(oldPassword, salt);
 
-                // If the old password is correct, then the password can
-                // be updated in the database with the new password
-                if (salted_hash == result.rows[0].saltedhash) {
-                    const newSalt = generateSalt(32)
-                    const newSaltedHash = generateHash(newPassword, newSalt)
-                    let insertNewPassQuery = "UPDATE CREDENTIALS SET SALT = $1, SALTEDHASH = $2 WHERE CREDENTIALS.MEMBERID = $3"
-                    const newValues = [newSalt, newSaltedHash, memberid]
-                    pool.query(insertNewPassQuery, newValues)
-                        .then(result => {
-                            // Succesfullu updated password
-                            if (result.rowCount == 1) {
-                                response.status(200).send({
-                                    message: 'Password updated'
-                                })
-                                return
-                            }
-                            // We should never reach this
-                            // If multiple accounts got their password updated
-                            // or none at all 
-                            else {
-                                response.status(400).send({
-                                    message: 'Error has occured'
-                                })
-                                return
-                            }
-                        })
-                        // If the query somehow fails
-                        .catch((error) => {
-                            response.status(500).send({
-                                message: 'Database query failed'
-                            })
-                            return
-                        })
-                }
-                // Invalid old password input by user
-                else {
-                    response.status(400).send({
-                        message: 'Invalid old password'
-                    })
-                    return
-                }
-            }
-        })
-        // Failed to ger users salted hash
-        .catch((error) => {
-            response.status(500).send({
-                message: 'Database query failed'
+        // If the old password is correct, then the password can
+        // be updated in the database with the new password
+        if (salted_hash == result.rows[0].saltedhash) {
+          const newSalt = generateSalt(32);
+          const newSaltedHash = generateHash(newPassword, newSalt);
+          let insertNewPassQuery =
+            "UPDATE CREDENTIALS SET SALT = $1, SALTEDHASH = $2 WHERE CREDENTIALS.MEMBERID = $3";
+          const newValues = [newSalt, newSaltedHash, memberid];
+          pool
+            .query(insertNewPassQuery, newValues)
+            .then((result) => {
+              // Succesfullu updated password
+              if (result.rowCount == 1) {
+                response.status(200).send({
+                  message: "Password updated",
+                });
+                return;
+              }
+              // We should never reach this
+              // If multiple accounts got their password updated
+              // or none at all
+              else {
+                response.status(400).send({
+                  message: "Error has occured",
+                });
+                return;
+              }
             })
-            return
-        })
+            // If the query somehow fails
+            .catch((error) => {
+              response.status(500).send({
+                message: "Database query failed #1",
+              });
+              return;
+            });
+        }
+        // Invalid old password input by user
+        else {
+          response.status(400).send({
+            message: "Invalid old password",
+          });
+          return;
+        }
+      }
+    })
+    // Failed to ger users salted hash
+    .catch((error) => {
+      response.status(500).send({
+        message: "Database query failed #2",
+      });
+      return;
+    });
+});
 
-})
-
-
-
-module.exports = router
+module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -9,36 +9,40 @@
  */
 
 //express is the framework we're going to use to handle requests
-const express = require('express')
+const express = require("express");
 
 // For email verification
-require('dotenv').config()
+require("dotenv").config();
 
-const nodemailer = require('nodemailer');
-const jwt = require('jsonwebtoken');
-const { isValidEmail, isValidPassword, isValidNickname } = require('../utilities/validationUtils');
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
+const {
+  isValidEmail,
+  isValidPassword,
+  isValidNickname,
+} = require("../utilities/validationUtils");
 
 //Access the connection to Heroku Database
-const pool = require('../utilities').pool
-const validation = require('../utilities').validation
-let isStringProvided = validation.isStringProvided
+const pool = require("../utilities").pool;
+const validation = require("../utilities").validation;
+let isStringProvided = validation.isStringProvided;
 
-const generateHash = require('../utilities').generateHash
-const generateSalt = require('../utilities').generateSalt
+const generateHash = require("../utilities").generateHash;
+const generateSalt = require("../utilities").generateSalt;
 
-const router = express.Router()
+const router = express.Router();
 
 /**
  * @api {post} /auth Request to register a user
  * @apiName PostAuth
  * @apiGroup Auth
- * 
+ *
  * @apiParam {String} first a users first name
  * @apiParam {String} last a users last name
  * @apiParam {String} username a username *unique
  * @apiParam {String} email a users email *unique
  * @apiParam {String} password a users password
- * 
+ *
  * @apiParamExample {json} Request-Body-Example:
  *  {
  *      "first":"testFirst",
@@ -47,155 +51,162 @@ const router = express.Router()
  *      "email":"testEmail@fake.email",
  *      "password":"test12345"
  *  }
- * 
+ *
  * @apiSuccess (Success 201) {boolean} success true when the name is inserted
- * @apiSuccess (Success 201) {String} email the email of the user inserted 
- * 
+ * @apiSuccess (Success 201) {String} email the email of the user inserted
+ *
  * @apiError (400: Missing Parameters) {String} message "Missing required information"
- * 
+ *
  * @apiError (400: Username exists) {String} message "Username exists"
- * 
+ *
  * @apiError (400: Email exists) {String} message "Email exists"
- *  
+ *
  * @apiError (400: Other Error) {String} message "other error, see detail"
  * @apiError (400: Other Error) {String} detail Information about the error
- * 
- */ 
-router.post('/', (request, response, next) => {
-
+ *
+ */
+router.post(
+  "/",
+  (request, response, next) => {
     //Retrieve data from query params
-    const first = request.body.first
-    const last = request.body.last
-    const username = isStringProvided(request.body.username) ?  request.body.username : request.body.email
-    const email = request.body.email
-    const password = request.body.password
-    
-
-    
+    const first = request.body.first;
+    const last = request.body.last;
+    const username = isStringProvided(request.body.username)
+      ? request.body.username
+      : request.body.email;
+    const email = request.body.email;
+    const password = request.body.password;
 
     //Verify that the caller supplied all the parameters
     //In js, empty strings or null values evaluate to false
-    if(isValidNickname(first) 
-        && isValidNickname(last) 
-        && isValidNickname(username) 
-        && isValidEmail(email) 
-        && isValidPassword(password)) {
-        
-        //We're using placeholders ($1, $2, $3) in the SQL query string to avoid SQL Injection
-        //If you want to read more: https://stackoverflow.com/a/8265319
-        let theQuery = "INSERT INTO MEMBERS(FirstName, LastName, Username, Email) VALUES ($1, $2, $3, $4) RETURNING Email, MemberID"
-        let values = [first, last, username, email]
-        pool.query(theQuery, values)
-            .then(result => {
-                //stash the memberid into the request object to be used in the next function
-                request.memberid = result.rows[0].memberid
-                
-                next()
-            })
-            .catch((error) => {
-                //log the error  for debugging
-                // console.log("Member insert")
-                // console.log(error)
-                if (error.constraint == "members_username_key") {
-                    response.status(400).send({
-                        message: "Username exists"
-                    })
-                } else if (error.constraint == "members_email_key") {
-                    response.status(400).send({
-                        message: "Email exists"
-                    })
-                } else {
-                    response.status(400).send({
-                        message: "other error, see detail",
-                        detail: error.detail
-                    })
-                }
-            })
-    } else {
-        response.status(400).send({
-            message: "Missing required information"
+    if (
+      isValidNickname(first) &&
+      isValidNickname(last) &&
+      isValidNickname(username) &&
+      isValidEmail(email) &&
+      isValidPassword(password)
+    ) {
+      //We're using placeholders ($1, $2, $3) in the SQL query string to avoid SQL Injection
+      //If you want to read more: https://stackoverflow.com/a/8265319
+      let theQuery =
+        "INSERT INTO MEMBERS(FirstName, LastName, Username, Email) VALUES ($1, $2, $3, $4) RETURNING Email, MemberID";
+      let values = [first, last, username, email];
+      pool
+        .query(theQuery, values)
+        .then((result) => {
+          //stash the memberid into the request object to be used in the next function
+          request.memberid = result.rows[0].memberid;
+
+          next();
         })
+        .catch((error) => {
+          //log the error  for debugging
+          // console.log("Member insert")
+          // console.log(error)
+          if (error.constraint == "members_username_key") {
+            response.status(400).send({
+              message: "Username exists",
+            });
+          } else if (error.constraint == "members_email_key") {
+            response.status(400).send({
+              message: "Email exists",
+            });
+          } else {
+            response.status(400).send({
+              message: "other error, see detail",
+              detail: error.detail,
+            });
+          }
+        });
+    } else {
+      response.status(400).send({
+        message: "Missing required information",
+      });
     }
-}, (request, response) => {
-        //We're storing salted hashes to make our application more secure
-        //If you're interested as to what that is, and why we should use it
-        //watch this youtube video: https://www.youtube.com/watch?v=8ZtInClXe1Q
-        let salt = generateSalt(32)
-        let salted_hash = generateHash(request.body.password, salt)
-        let theQuery = "INSERT INTO CREDENTIALS(MemberId, SaltedHash, Salt) VALUES ($1, $2, $3)"
-        let values = [request.memberid, salted_hash, salt]
-        pool.query(theQuery, values)
-            .then(result => {
-                // Creating email to send to user for verification
-                const transporter = nodemailer.createTransport({
-                    host: 'smtp.gmail.com',
-                    port: 587,
-                    protocol: 'tls', 
-                    service: 'gmail',
-                    secure: false,
-                    auth: {
-                        user: process.env.EMAIL,
-                        pass: process.env.PASSWORD
-                    },
-                    debug: false,
-                    logger: true
-                });
-                  
-                const token = jwt.sign({
-                        memberid: request.memberid,
-                    }, process.env.JSON_WEB_TOKEN, { expiresIn: '10m' }  
-                );    
-                const mailConfigurations = {
-      
-                    // It should be a string of sender/server email
-                    from: process.env.EMAIL,
-                  
-                    to: request.body.email,
-                  
-                    // Subject of Email
-                    subject: 'Email Verification',
-                      
-                    // This would be the text of email body
-                    text: `Hi! To register please follow the link https://tcss-450-sp22-group-8.herokuapp.com/verify/${token}`
-                      
-                };
-                transporter.sendMail(mailConfigurations, function(error, info){
-                    if (error) {
-                        console.log(error)
-                        response.status(401).send({
-                        message: "Error sending email, possible incorrect email" + error});
-                        console.log(error)
-                    } else {
-                        //We successfully added the user!
-                        response.status(201).send({
-                            success: true,
-                            email: request.body.email
-                        })
-                        console.log('Email Sent Successfully');
-                        console.log(info);
-                    }
-                });
-                return
-            })
-            .catch((error) => {
-                //log the error for debugging
-                // console.log("PWD insert")
-                console.log(error)
+  },
+  (request, response) => {
+    //We're storing salted hashes to make our application more secure
+    //If you're interested as to what that is, and why we should use it
+    //watch this youtube video: https://www.youtube.com/watch?v=8ZtInClXe1Q
+    let salt = generateSalt(32);
+    let salted_hash = generateHash(request.body.password, salt);
+    let theQuery =
+      "INSERT INTO CREDENTIALS(MemberId, SaltedHash, Salt) VALUES ($1, $2, $3)";
+    let values = [request.memberid, salted_hash, salt];
+    pool
+      .query(theQuery, values)
+      .then((result) => {
+        // Creating email to send to user for verification
+        const transporter = nodemailer.createTransport({
+          host: "smtp.gmail.com",
+          port: 587,
+          protocol: "tls",
+          service: "gmail",
+          secure: false,
+          auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+          },
+          debug: false,
+          logger: true,
+        });
 
-                /***********************************************************************
-                 * If we get an error inserting the PWD, we should go back and remove
-                 * the user from the member table. We don't want a member in that table
-                 * without a PWD! That implementation is up to you if you want to add
-                 * that step. 
-                 **********************************************************************/
+        const token = jwt.sign(
+          {
+            memberid: request.memberid,
+          },
+          process.env.JSON_WEB_TOKEN,
+          { expiresIn: "10m" }
+        );
+        const mailConfigurations = {
+          // It should be a string of sender/server email
+          from: process.env.EMAIL,
 
-                response.status(400).send({
-                    message: "other error, see detail",
-                    detail: error.detail
-                })
-            })
-        
-})
+          to: request.body.email,
 
+          // Subject of Email
+          subject: "Email Verification",
 
-module.exports = router
+          // This would be the text of email body
+          text: `Hi! To register please follow the link https://tcss-450-sp22-group-8.herokuapp.com/verify/${token}`,
+        };
+        transporter.sendMail(mailConfigurations, function (error, info) {
+          if (error) {
+            console.log(error);
+            response.status(401).send({
+              message: "Error sending email, possible incorrect email" + error,
+            });
+            console.log(error);
+          } else {
+            //We successfully added the user!
+            response.status(201).send({
+              success: true,
+              email: request.body.email,
+            });
+            console.log("Email Sent Successfully");
+            console.log(info);
+          }
+        });
+        return;
+      })
+      .catch((error) => {
+        //log the error for debugging
+        // console.log("PWD insert")
+        console.log(error);
+
+        /***********************************************************************
+         * If we get an error inserting the PWD, we should go back and remove
+         * the user from the member table. We don't want a member in that table
+         * without a PWD! That implementation is up to you if you want to add
+         * that step.
+         **********************************************************************/
+
+        response.status(400).send({
+          message: "other error, see detail",
+          detail: error.detail,
+        });
+      });
+  }
+);
+
+module.exports = router;
