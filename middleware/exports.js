0504d577de536d83a83b0f89d8fd42b7d285b651
slogansUW <slogans@uw.edu>
2022-05-18 23:44:06 -0700

    pushy stuff

--- middleware/exports.js
+++ middleware/exports.js
@@ -1,6 +1,7 @@
-let checkToken = require('./jwt.js').checkToken
-let jsonErrorInBody = require('./handleErrors.js').jsonErrorInBody
+let checkToken = require("./jwt.js").checkToken;
+let jsonErrorInBody = require("./handleErrors.js").jsonErrorInBody;
 
 module.exports = {
-    checkToken, jsonErrorInBody
-}
+    checkToken,
+    jsonErrorInBody,
+};
